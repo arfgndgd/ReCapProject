4183df773b9b64d0e3db4ae8bd06027cb8163e7f
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-04-24 01:43:40 +0300

    CarManager's class modified for new implement

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -41,32 +41,37 @@ namespace Business.Concrete
 
         public IDataResult<List<Car>> GetAll()
         {
-            return _carDal.GetAll();
+            if (DateTime.Now.Hour == 22)
+            {
+                //uydurma bi kod (örn: saat 22den sonra listeleme kapalı)
+                return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);//bakım zamanı
+            }
+            return new SuccessDataResult<List<Car>> (_carDal.GetAll(),Messages.CarsListed);
         }
 
         public IDataResult<List<Car>> GetByDailyPrice(double min, double max)
         {
-            return _carDal.GetAll(c=>c.DailyPrice >= min && c.DailyPrice <= max);
+            return new SuccessDataResult<List<Car>> (_carDal.GetAll(c=>c.DailyPrice >= min && c.DailyPrice <= max));
         }
 
         public IDataResult<Car> GetById(int carId)
         {
-            return _carDal.Get(c=>c.CarID == carId);
+            return new SuccessDataResult<Car> (_carDal.Get(c=>c.CarID == carId));
         }
 
         public IDataResult<List<CarDetailDto>> GetCarDetails()
         {
-            return _carDal.GetCarDetails();
+            return new SuccessDataResult<List<CarDetailDto>> (_carDal.GetCarDetails());
         }
 
         public IDataResult<List<Car>> GetCarsByBrandId(int id)
         {
-            return new SuccessDataResult (_carDal.GetAll(c=>c.BrandID == id));
+            return new SuccessDataResult<List<Car>> (_carDal.GetAll(c=>c.BrandID == id));
         }
 
         public IDataResult<List<Car>> GetCarsByColorId(int id)
         {
-            return _carDal.GetAll(c => c.ColorID == id);
+            return new SuccessDataResult<List<Car>> (_carDal.GetAll(c => c.ColorID == id));
         }
 
         public void Update(Car car)
