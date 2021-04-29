767246f4f63ddd04bd118d8993c6a68da00bec57
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-04-29 18:18:45 +0300

    Autofac added

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -32,9 +32,11 @@ namespace WebAPI
         {
 
             services.AddControllers();
-            services.AddSingleton<ICarService,CarManager>();
-            services.AddSingleton<ICarDal, EfCarDal>();
-           
+            #region Autofacsiz
+            //services.AddSingleton<ICarService,CarManager>();
+            //services.AddSingleton<ICarDal, EfCarDal>(); 
+            #endregion
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
